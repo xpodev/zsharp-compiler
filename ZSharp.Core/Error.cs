7add621a27a68367930d990376a42998f7c09c41
Binyamin Y Cohen <binyamincohen555@gmail.com>
2022-02-01 21:19:56 +0200

    added Error wrapper class

---
+++ ZSharp.Core/Error.cs
@@ -0,0 +1,26 @@
+﻿namespace ZSharp.Core
+{
+    public struct Error
+    {
+        public string Message { get; }
+
+        public ObjectInfo Object { get; }
+
+        public FileInfo FileInfo => Object.FileInfo;
+
+        public Error(string message) : this(message, null) { }
+
+        public Error(string message, ObjectInfo info)
+        {
+            Message = message;
+            Object = info;
+        }
+
+        public Error With(ObjectInfo info) => new(Message, info);
+
+        public override string ToString()
+        {
+            return $"({FileInfo.StartLine}, {FileInfo.StartColumn}): {Message}";
+        }
+    }
+}
