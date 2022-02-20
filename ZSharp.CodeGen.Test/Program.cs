2a6b7b26fe22df14affbf9e09d68dca2425a8bba
Binyamin Y Cohen <binyamincohen555@gmail.com>
2022-02-20 06:31:32 +0200

    Add small CodeGen test

---
+++ ZSharp.CodeGen.Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using ZSharp.CG;
+
+namespace ZSharp.CodeGen.Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ModuleBuilder module = new("TestModule");
+
+            var someType = module.DefineType("SomeType");
+            var someMethod = someType.DefineMethod("SomeMethod", someType);
+            someMethod.DefineParameter("someParameter", someType);
+            someType.DefineField("someField", someType);
+
+            module.DefineFunction("SomeGlobalFunction", someType);
+
+            module.Write("Test.dll");
+        }
+    }
+}
