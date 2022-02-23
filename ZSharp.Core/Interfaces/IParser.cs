8f67d4e23193c1dee24e81faea5a5cf6e39901e4
Binyamin Y Cohen <binyamincohen555@gmail.com>
2022-02-23 03:52:25 +0200

    Update IParser.cs

--- ZSharp.Core/Interfaces/IParser.cs
+++ ZSharp.Core/Interfaces/IParser.cs
@@ -1,12 +1,9 @@
 ﻿using System.Collections.Generic;
-using System.IO;
 
 namespace ZSharp.Core
 {
     public interface IParser
     {
-        IEnumerable<ObjectInfo> Parse(TextReader input);
-
-        public void SetDocument(DocumentInfo document);
+        IEnumerable<ObjectInfo> ParseFile(string filePath);
     }
 }
