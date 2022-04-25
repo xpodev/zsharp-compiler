4399d66b7f56f9bad339f7c36d63b6060f55a519
Binyamin Y Cohen <binyamincohen555@gmail.com>
2022-04-25 19:57:24 +0300

    Add documentation

--- ZSharp.Core/FileInfo.cs
+++ ZSharp.Core/FileInfo.cs
@@ -2,18 +2,44 @@
 
 namespace ZSharp.Core
 {
+    /// <summary>
+    /// Contains span information in a certain document.
+    /// </summary>
     public struct FileInfo
     {
+        /// <summary>
+        /// The document info.
+        /// </summary>
         public DocumentInfo Document { get; }
 
+        /// <summary>
+        /// The line at which the span starts.
+        /// </summary>
         public int StartLine { get; }
 
+        /// <summary>
+        /// The line at which the span ends.
+        /// </summary>
         public int EndLine { get; }
 
+        /// <summary>
+        /// The column at which the span starts.
+        /// </summary>
         public int StartColumn { get; }
 
+        /// <summary>
+        /// The column at which the span ends.
+        /// </summary>
         public int EndColumn { get; }
 
+        /// <summary>
+        /// Creates a new FileInfo instance.
+        /// </summary>
+        /// <param name="document">The document that contains the span.</param>
+        /// <param name="startLine">The line at which the span starts.</param>
+        /// <param name="startColumn">The column at which the span starts.</param>
+        /// <param name="endLine">The line at which the span ends.</param>
+        /// <param name="endColumn">The column at which the span ends.</param>
         public FileInfo(DocumentInfo document, int startLine, int startColumn, int endLine, int endColumn)
         {
             Document = document;
@@ -23,6 +49,13 @@ namespace ZSharp.Core
             EndColumn = endColumn;
         }
 
+        /// <summary>
+        /// Combines two FileInfo instances to include both spans. The order of parameters does not matter.
+        /// </summary>
+        /// <param name="a">The first span.</param>
+        /// <param name="b">The second span.</param>
+        /// <returns>A new FileInfo for a span that includes both span and everything in between them.</returns>
+        /// <exception cref="ArgumentException">Thrown when the 2 spans are in different documents.</exception>
         public static FileInfo Combine(FileInfo a, FileInfo b)
         {
             if (a.Document != b.Document)
@@ -54,6 +87,11 @@ namespace ZSharp.Core
             return base.Equals(obj);
         }
 
+        /// <summary>
+        /// Checks if two FileInfo instances are equal.
+        /// </summary>
+        /// <param name="other">The other file info.</param>
+        /// <returns><see cref="true"/> if the 2 infos represent the same span in the same file, <see cref="false"/> otherwise.</returns>
         public bool Equals(ref FileInfo other) =>
             Document == other.Document &&
             StartLine == other.StartLine &&
